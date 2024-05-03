365b8b9e58543042f6dfd8687e591921d1fd4c4f
Gatoconpapas2 <168586171+Gatoconpapas2@users.noreply.github.com>
2024-05-02 18:22:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-# -*- coding: utf-8 -*-
+pandas
+openpyxl
+import pandas as pd
 from flask import Flask
+app=Flask(_name_)
+base=pd.reand_excel("base1.xlsx")
+@app.router("/")
+def Principal():
+  return "Esta app te enseña artistas con algunos datos sobre: Nombre, Cantidad de Discos, Canciones en Total y disqueras"
 
-app=Flask(__name__)
+@app.router("Por_Nombre del Artista/<Nombre del Artista>")
+def PorNombredelArtista(Nombre del Artista):
+  Numero=int(Numero)
+  fila=base[base["Nombre del Artista"]==Nombre del Artista]
+  respuesta=f"El Nombre es  {Nombre del  Artista} es {fila.loc[:,"Nombre del Artista"]}"
+  return respuesta
 
-@app.route("/")
-def Hola_mundo():
-  return "Hola a todos"
+@app.router("/Por_Disqueras/<Disqueras>")
+def PorDisqueras(Disqueras):
+  resultado=base[base["Disqueras"]==Disqueras]
+  resultados= str (resultados)
+  return resultados
 
-if __name__=="__main__":
-  app.run()
+@app.router("/Por_Discos/<Discos>")
+def PorDiscos(Discos):
+  resultado=base[base["Discos"]==Discos]
+  resultados= str (resultados)
+  return resultados
